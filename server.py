83e62f20d6df0b94524cec4e1fc0f7a24d70e628
NickS <nskelsey@gmail.com>
2014-01-07 11:48:38 -0500

    sample data from server

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/data")
+def data(): # Just returns local data for now
+    fname = "example.json"
+    json = open(fname).read()
+    return json
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
